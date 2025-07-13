6902f60a511424e8e713ec2bc9eb04068a86369b
joulook <mkhoshechin@mpi-sws.org>
2025-07-13 22:23:56 +0000

    Deploying to gh-pages from @ joulook/joulook.github.io@a20d8dfd4e869b723e4b1616aeb55e296a9a0cbe 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,35 +438,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6D%6B%68%6F%73%68%65%63%68%69%6E@%6D%70%69-%73%77%73.%6F%72%67", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/joulook# your GitHub user name", "_blank");
         },
       },{
       id: 'light-theme',
